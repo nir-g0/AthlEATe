ff3238fdac6272f5617a84d71a19c7083e03786c
nir-g0 <ngub24@gmail.com>
2024-11-11 16:48:11 -0800

    Meals UI Updates

---
+++ Pages/MealsScreen/MealsScreen.tsx
@@ -0,0 +1,184 @@
+import {
+  View,
+  Text,
+  TouchableOpacity,
+  StyleSheet,
+  FlatList,
+  Button,
+  Dimensions
+} from 'react-native'
+import { SafeAreaView } from 'react-native'
+import DefaultPage from '../../ConstantStyles/DefaultPage'
+import AppStyles from '../../ConstantStyles/Styles'
+import React, { useEffect, useState } from 'react'
+
+const { width, height } = Dimensions.get('window')
+
+function MealsScreen ({ navigation }: { navigation: any }): React.JSX.Element {
+  const [selectedDay, setSelectedDay] = useState(0)
+  const [orderedDays, setOrderedDays] = useState<string[]>([])
+  const [meals, setMeals] = useState([])
+
+  const daysOfTheWeek = ['MON', 'TUE', 'WED', 'THU', 'FRI', 'SAT', 'SUN']
+
+  const mealOptions = [
+    { name: 'Chicken Wrap', calories: 420, protein: 25, carbs: 40, fat: 12 },
+    {
+      name: 'Grilled Chicken Salad',
+      calories: 450,
+      protein: 30,
+      carbs: 20,
+      fat: 15
+    },
+    { name: 'Protein Smoothie', calories: 300, protein: 20, carbs: 35, fat: 5 },
+    { name: 'Quinoa Bowl', calories: 500, protein: 25, carbs: 50, fat: 10 },
+    { name: 'Spaghetti Bowl', calories: 800, protein: 25, carbs: 50, fat: 10 },
+    {
+      name: 'Salmon & Veggies',
+      calories: 480,
+      protein: 35,
+      carbs: 15,
+      fat: 18
+    },
+    {
+      name: 'Oatmeal with Berries',
+      calories: 350,
+      protein: 10,
+      carbs: 60,
+      fat: 8
+    }
+  ]
+
+  useEffect(() => {
+    const today = new Date().getDay()
+    const adjustedDays = [
+      ...daysOfTheWeek.slice(today === 0 ? 6 : today - 1),
+      ...daysOfTheWeek.slice(0, today === 0 ? 6 : today - 1)
+    ]
+    setOrderedDays(adjustedDays)
+    generateRandomMeals()
+  }, [])
+
+  const generateRandomMeals = () => {
+    const randomMeals = []
+    for (let i = 0; i < 3; i++) {
+      const randomMeal =
+        mealOptions[Math.floor(Math.random() * mealOptions.length)]
+      randomMeals.push({ ...randomMeal, id: `${i}-${randomMeal.name}` })
+    }
+    setMeals(randomMeals)
+  }
+
+  useEffect(() => {
+    generateRandomMeals()
+  }, [selectedDay])
+
+  const handleGenerateDay = lst => {
+    return orderedDays.map((day, index) => (
+      <TouchableOpacity
+        key={day}
+        style={{
+          ...styles.dayBubble,
+          backgroundColor: index === selectedDay ? '#42D951' : '#F0F0F0'
+        }}
+        onPress={() => setSelectedDay(index)}
+      >
+        <Text
+          style={{
+            fontSize: width * 0.035, // Responsive font size
+            color: index === selectedDay ? 'white' : 'black',
+            fontWeight: '600',
+            fontFamily: 'Menlo'
+          }}
+        >
+          {day}
+        </Text>
+      </TouchableOpacity>
+    ))
+  }
+
+  const renderMealCard = ({ item }) => (
+    <View style={styles.mealCard}>
+      <Text style={styles.mealTitle}>{item.name}</Text>
+      <Text style={styles.mealDetails}>Calories: {item.calories}</Text>
+      <Text style={styles.mealDetails}>
+        Protein: {item.protein}g | Carbs: {item.carbs}g | Fat: {item.fat}g
+      </Text>
+    </View>
+  )
+
+  return (
+    <SafeAreaView style={{ ...AppStyles.defaultBackground, flex: 1 }}>
+      <DefaultPage navigation={navigation} title='Meals'>
+        <View style={styles.bubbleContainer}>
+          {handleGenerateDay([0, 1, 2, 3, 4, 5, 6])}
+        </View>
+        <FlatList
+          data={meals}
+          renderItem={renderMealCard}
+          keyExtractor={item => item.id}
+          style={{ width: '95%' }}
+          contentContainerStyle={{ paddingHorizontal: width * 0.04 }}
+        />
+        <View style={styles.regenerateButtonContainer}>
+          <Button
+            title={'Regenerate'}
+            onPress={generateRandomMeals}
+            color='#007AFF'
+          />
+        </View>
+      </DefaultPage>
+    </SafeAreaView>
+  )
+}
+
+const styles = StyleSheet.create({
+  mealCard: {
+    backgroundColor: '#ffffff',
+    shadowColor: '#000',
+    shadowOpacity: 0.2,
+    shadowRadius: 3,
+    shadowOffset: { width: 0, height: 2 },
+    borderRadius: width * 0.03, // Responsive border radius
+    padding: width * 0.04,
+    marginVertical: height * 0.005,
+    marginHorizontal: width * 0.01,
+    alignItems: 'flex-start'
+  },
+  mealTitle: {
+    fontSize: width * 0.045, // Responsive font size
+    fontWeight: '600',
+    color: '#333',
+    marginBottom: height * 0.005,
+    fontFamily: 'Menlo'
+  },
+  mealDetails: {
+    fontSize: width * 0.0325, // Responsive font size
+    color: '#666',
+    fontFamily: 'Menlo'
+  },
+  bubbleContainer: {
+    flexDirection: 'row',
+    justifyContent: 'space-around',
+    paddingVertical: height * 0.01,
+    paddingHorizontal: width * 0.05
+  },
+  dayBubble: {
+    height: width * 0.12,
+    width: width * 0.12,
+    borderRadius: width * 0.04, // Circle
+    justifyContent: 'center',
+    alignItems: 'center',
+    shadowColor: '#000',
+    shadowOpacity: 0.25,
+    shadowRadius: 3,
+    shadowOffset: { width: 0, height: 1 },
+    marginHorizontal: width * 0.01
+  },
+  regenerateButtonContainer: {
+    alignItems: 'center',
+    marginVertical: height * 0.02
+  }
+})
+
+export default MealsScreen
